bc3043c0f8bb51bb0dbec73bdd192ec522ea43ed
Sergey <serridd@gmail.com>
2021-01-15 22:48:08 +0300

    Debouncing or something idk.

--- static/res/main.js
+++ static/res/main.js
@@ -1,6 +1,31 @@
 const identity = (a) => a
 
+const log = (f) => (...params) => {
+    console.log('a')
+    f(...params)
+}
+
 const saveScroll = () => {
+    // const debounce = (fn) => {
+    //     // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+    //     let frame;
+    //
+    //     // The debounce function returns a new function that can receive a variable number of arguments
+    //     return (...params) => {
+    //         // If the frame variable has been defined, clear it now, and queue for next frame
+    //         if (frame) {
+    //             cancelAnimationFrame(frame);
+    //         }
+
+    //         // Queue our function call for the next frame
+    //         frame = requestAnimationFrame(() => {
+
+    //             // Call our function and pass any params we received
+    //             fn(...params);
+    //         });
+    //     }
+    // };
+
     // don't debounce
     const debounce = identity
 
@@ -10,7 +35,7 @@ const saveScroll = () => {
 
     storeScroll()
 
-    document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+    document.addEventListener('scroll', debounce(log(storeScroll)), { passive: true });
 
     window.onload = () => {
         setTimeout(() => {
@@ -21,7 +46,6 @@ const saveScroll = () => {
 
 const main = () => {
     saveScroll()
-    console.log('a')
 }
 
 main()
